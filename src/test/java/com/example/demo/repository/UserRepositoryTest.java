e670f2a886fcba24fdc5832336b2c34543bd7dda
nkumarcaes <naveenkumar.parameswaran@hmhco.com>
2018-07-04 16:57:37 +0100

    Test case fix - Spring

--- src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -3,13 +3,18 @@ package com.example.demo.repository;
 import com.example.demo.domain.User;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@Import({UserRepository.class})
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class UserRepositoryTest extends Neo4jIntegrationTest {
 
     @Autowired
